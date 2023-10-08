f01178cda8c8ca9e9d7775b1b393254733ff82d7
noredev <noredeveloper@gmail.com>
2023-10-08 20:27:46 +0300

    feat: implement countdown timer component

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,11 @@
+<script>
+	import Countdown from "./Countdown.svelte";
+	import Pomodoro from "./components/Pomodoro.svelte";
+</script>
+
 <main>
-  <h1>Hello, World!</h1>
+	<h1>Hello, World!</h1>
+	<Pomodoro />
+	<Countdown date={new Date("2024-06-15T10:15:00")} name={"TYT"} />
+	<Countdown date={new Date("2024-06-16T10:15:00")} name={"AYT/YDT"} />
 </main>
---
+++ src/Countdown.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	export let date: Date;
+	export let name: string;
+
+	const current_date = Date.now();
+
+	function update_date(date: Date) {
+		date.setSeconds(date.getSeconds() - 1);
+
+		return date;
+	}
+
+	setInterval(() => {
+		date = update_date(date);
+	}, 1000);
+
+	function get_remaining_days(date: Date, current_date: number) {
+		const difference_in_milliseconds = date.getTime() - current_date;
+		return Math.floor(difference_in_milliseconds / (1000 * 60 * 60 * 24));
+	}
+
+	function get_remaining_hours(date: Date, current_date: number) {
+		const difference_in_milliseconds = date.getTime() - current_date;
+		return Math.floor((difference_in_milliseconds / (1000 * 60 * 60)) % 24);
+	}
+
+	function get_remaining_minutes(date: Date, current_date: number) {
+		const difference_in_milliseconds = date.getTime() - current_date;
+		return Math.floor((difference_in_milliseconds / (1000 * 60)) % 60);
+	}
+
+	function get_remaining_seconds(date: Date, current_date: number) {
+		const difference_in_milliseconds = date.getTime() - current_date;
+		return Math.floor((difference_in_milliseconds / 1000) % 60);
+	}
+</script>
+
+<main>
+	<p>{name}'ye</p>
+
+	<div class="container">
+		<p>{get_remaining_days(date, current_date)} <br />gun</p>
+		<p>{get_remaining_hours(date, current_date)} <br />saat</p>
+		<p>{get_remaining_minutes(date, current_date)} <br />dakika</p>
+		<p>{get_remaining_seconds(date, current_date)} <br />saniye</p>
+	</div>
+
+	<p>kaldi</p>
+</main>
+
+<style lang="css">
+	main {
+		text-align: center;
+		margin: 10px;
+		background-color: gray;
+		width: 300px;
+	}
+
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+	}
+</style>
