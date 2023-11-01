0dbc1f90baf893cf88fb413bd19ad0428f21c0ab
noredev <noredeveloper@gmail.com>
2023-11-01 21:27:06 +0300

    gereksiz kod kaldirildi

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { addRxPlugin } from 'rxdb'
-import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode'
 
-let isDevMode = true
-
-if (isDevMode) {
-	addRxPlugin(RxDBDevModePlugin)
-}
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
